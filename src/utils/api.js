4b1908b6495a312b64e2243a7df129791dcbe710
Jon Eastin <picardncce@gmail.com>
2022-07-27 15:09:46 -0700

    reworking api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,15 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL =
+	process.env.API_BASE_URL ||
+	'git@github.com:JonEast87/starter-grub-dash-front-end.git'
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
+const headers = new Headers()
+headers.append('Content-Type', 'application/json')
 
 /**
  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
@@ -24,25 +26,25 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options) {
-  try {
-    const response = await fetch(url, options);
+	try {
+		const response = await fetch(url, options)
 
-    if (response.status === 204) {
-      return null;
-    }
+		if (response.status === 204) {
+			return null
+		}
 
-    const payload = await response.json();
+		const payload = await response.json()
 
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
-    }
-  }
+		if (payload.error) {
+			return Promise.reject({ message: payload.error })
+		}
+		return payload.data
+	} catch (error) {
+		if (error.name !== 'AbortError') {
+			console.error(error.stack)
+			throw error
+		}
+	}
 }
 
 /**
@@ -51,8 +53,8 @@ async function fetchJson(url, options) {
  *  a promise that resolves to a possibly empty array of dishes saved in the database.
  */
 export async function listDishes(signal) {
-  const url = `${API_BASE_URL}/dishes`;
-  return await fetchJson(url, { signal });
+	const url = `${API_BASE_URL}/dishes`
+	return await fetchJson(url, { signal })
 }
 
 /**
@@ -61,14 +63,14 @@ export async function listDishes(signal) {
  *  a promise that resolves to the newly created order.
  */
 export async function createOrder(order, signal) {
-  const url = `${API_BASE_URL}/orders`;
-  const options = {
-    method: "POST",
-    headers,
-    body: JSON.stringify({ data: order }),
-    signal,
-  };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/orders`
+	const options = {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({ data: order }),
+		signal,
+	}
+	return await fetchJson(url, options)
 }
 
 /**
@@ -81,8 +83,8 @@ export async function createOrder(order, signal) {
  *  a promise that resolves to the saved order.
  */
 export async function readOrder(orderId, signal) {
-  const url = `${API_BASE_URL}/orders/${orderId}`;
-  return await fetchJson(url, { signal });
+	const url = `${API_BASE_URL}/orders/${orderId}`
+	return await fetchJson(url, { signal })
 }
 
 /**
@@ -91,8 +93,8 @@ export async function readOrder(orderId, signal) {
  *  a promise that resolves to a possibly empty array of orders saved in the database.
  */
 export async function listOrders(signal) {
-  const url = `${API_BASE_URL}/orders`;
-  return await fetchJson(url, { signal });
+	const url = `${API_BASE_URL}/orders`
+	return await fetchJson(url, { signal })
 }
 
 /**
@@ -101,14 +103,14 @@ export async function listOrders(signal) {
  *  a promise that resolves to the update order.
  */
 export async function updateOrder(order, signal) {
-  const url = `${API_BASE_URL}/orders/${order.id}`;
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify({ data: order }),
-    signal,
-  };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/orders/${order.id}`
+	const options = {
+		method: 'PUT',
+		headers,
+		body: JSON.stringify({ data: order }),
+		signal,
+	}
+	return await fetchJson(url, options)
 }
 
 /**
@@ -121,9 +123,9 @@ export async function updateOrder(order, signal) {
  *  a promise that resolves to null or an error message.
  */
 export async function deleteOrder(orderId, signal) {
-  const url = `${API_BASE_URL}/orders/${orderId}`;
-  const options = { method: "DELETE", signal };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/orders/${orderId}`
+	const options = { method: 'DELETE', signal }
+	return await fetchJson(url, options)
 }
 
 /**
@@ -132,14 +134,14 @@ export async function deleteOrder(orderId, signal) {
  *  a promise that resolves to the newly created dish.
  */
 export async function createDish(dish, signal) {
-  const url = `${API_BASE_URL}/dishes`;
-  const options = {
-    method: "POST",
-    headers,
-    body: JSON.stringify({ data: dish }),
-    signal,
-  };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/dishes`
+	const options = {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({ data: dish }),
+		signal,
+	}
+	return await fetchJson(url, options)
 }
 
 /**
@@ -152,8 +154,8 @@ export async function createDish(dish, signal) {
  *  a promise that resolves to the saved dish.
  */
 export async function readDish(dishId, signal) {
-  const url = `${API_BASE_URL}/dishes/${dishId}`;
-  return await fetchJson(url, { signal });
+	const url = `${API_BASE_URL}/dishes/${dishId}`
+	return await fetchJson(url, { signal })
 }
 
 /**
@@ -162,14 +164,14 @@ export async function readDish(dishId, signal) {
  *  a promise that resolves to the updated dish.
  */
 export async function updateDish(dish, signal) {
-  const url = `${API_BASE_URL}/dishes/${dish.id}`;
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify({ data: dish }),
-    signal,
-  };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/dishes/${dish.id}`
+	const options = {
+		method: 'PUT',
+		headers,
+		body: JSON.stringify({ data: dish }),
+		signal,
+	}
+	return await fetchJson(url, options)
 }
 
 /**
@@ -182,7 +184,7 @@ export async function updateDish(dish, signal) {
  *  a promise that resolves to null or an error message.
  */
 export async function deleteDish(dishId, signal) {
-  const url = `${API_BASE_URL}/dishes/${dishId}`;
-  const options = { method: "DELETE", signal };
-  return await fetchJson(url, options);
+	const url = `${API_BASE_URL}/dishes/${dishId}`
+	const options = { method: 'DELETE', signal }
+	return await fetchJson(url, options)
 }
